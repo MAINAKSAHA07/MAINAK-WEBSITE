8197703617fdf31eb2df0719fae1d9aab5bdb9e0
MAINAK_SAHA <84928246+MAINAKSAHA07@users.noreply.github.com>
2025-01-09 20:36:51 -0700

    commit

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -82,10 +82,10 @@ const Projects = () => {
                     <Github className="w-5 h-5 mr-1" />
                     Code
                   </a>
-                /*  <a href="#" className="inline-flex items-center text-slate-600 hover:text-slate-900">
+                 {/*<a href="#" className="inline-flex items-center text-slate-600 hover:text-slate-900">
                     <ExternalLink className="w-5 h-5 mr-1" />
                     Demo
-                  </a> */
+                  </a> //*/}
                 </div>
               </div>
             </div>
